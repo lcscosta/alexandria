8a92274da7c39c7638ebc6f8d9a03c32686e44ea
lcscosta <lcscosta@disroot.org>
2025-04-01 22:58:32 -0300

    Added FastAPI starting app

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite qualquer origem
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Alexandria!"}
