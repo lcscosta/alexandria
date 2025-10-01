861d2476eae07e749ecb3b19b9fafc12f70875c3
lcscosta <lcscosta@disroot.org>
2025-09-30 23:46:39 -0300

    feat: changed next.js export to static HTML

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",       // static HTML export
+  distDir: "out",         // Output directory
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",           //  Set if deploying to a subpath
+  assetPrefix: "",        // CDN prefix for assets
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
